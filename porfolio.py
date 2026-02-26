37730aaf80ed42a29b71a2cf9b0518af281aaab6
dioufbirane02 <dioufbirane022@gmail.com>
2026-02-26 14:57:14 +0000

    Update porfolio.py

--- porfolio.py
+++ porfolio.py
@@ -4,10 +4,7 @@ with st.sidebar:
   st.write("Email:dioufbirane022@gmail.com")
   st.write("Adresse: Mbour Sérère Kao")
   
-  st.title("BIRANE mange du mangue Birane boit du riz Alors Birane a jeune Birane est derriere moi
-  quelle est la finalite; c est de vendre: entre le client et le produit qui est le plus important: 
-  Birane a dsit que cùest le produit le plus important parceque il a melange plusieurs truc")
-
-
-
+st.write("BIRANE mange du mangue Birane boit du riz")
+st.write("lors Birane a jeune Birane est derriere moi")  
+st.write("quelle est la finalite; c est de vendre") 
 
