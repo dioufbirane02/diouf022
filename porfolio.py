7349c25a972e9c5ff4c356871c78d3238b628106
dioufbirane02 <dioufbirane022@gmail.com>
2026-02-26 14:45:45 +0000

    Update porfolio.py

--- porfolio.py
+++ porfolio.py
@@ -8,5 +8,6 @@ with st.sidebar:
   Alors Birane a jeune 
   Birane est derriere moi
   quelle est la finalite; c est de vendre: entre le client et le produit qui est le plus important: 
-  Birane a dsit que cùest le produit le plus important parceque il a melange plusieurs truc"
+  Birane a dsit que cùest le produit le plus important parceque il a melange plusieurs truc")
+
 
