9c15ae20ac38a6c272e5c0166ca97b411bd3518e
dioufbirane02 <dioufbirane022@gmail.com>
2026-02-26 15:16:34 +0000

    Update porfolio.py

--- porfolio.py
+++ porfolio.py
@@ -4,7 +4,6 @@ with st.sidebar:
   st.write("Email:dioufbirane022@gmail.com")
   st.write("Adresse: Mbour Sérère Kao")
   
-st.write("BIRANE mange du mangue Birane boit du riz")
-st.write("lors Birane a jeune Birane est derriere moi")  
-st.write("quelle est la finalite; c est de vendre") 
+st.write("CURRICULUM VITAE")
+
 
